d37af61d2c35e2533e793ee370396f3c8053879d
huaixu <huaixu129@gmail.com>
2025-05-04 10:15:18 +0800

    功能搞定

--- src/components/graph1.vue
+++ src/components/graph1.vue
@@ -1,13 +1,35 @@
 <template lang="">
   <div class="container">
     <img src="../assets/img/bg1.png" alt="bg" />
-    <div class="cell1">主持人</div>
-    <div class="cell2">开凿时间</div>
-    <div class="cell3">工程面积（长度）</div>
+    <div class="cell1">{{ projectInfo.设计者 || '设计者' }}</div>
+    <div class="cell2">{{ projectInfo.修建时间 || '修建时间' }}</div>
+    <div class="cell3">{{ projectInfo.规模描述 || '规模描述' }}</div>
   </div>
 </template>
 <script>
-export default {};
+import { ref, inject, watch } from 'vue';
+import overviewData from '../data/overview.json';
+
+export default {
+  setup() {
+    // 注入悬浮项目状态
+    const hoveredProject = inject('hoveredProject', ref(null));
+    const projectInfo = ref({});
+
+    // 监听悬浮项目变化
+    watch(hoveredProject, (newValue) => {
+      if (newValue && overviewData[newValue]) {
+        projectInfo.value = overviewData[newValue];
+      } else {
+        projectInfo.value = {};
+      }
+    });
+
+    return {
+      projectInfo
+    };
+  }
+};
 </script>
 <style>
 .container {
@@ -25,8 +47,10 @@ export default {};
   z-index: 2; /* 设置 z-index 高于图片 */
   width: 50px; /* 设置宽度 */
   height: 125px; /* 设置高度 */
-  border: 2px solid #000; /* 添加边框 */
-  text-align: center; /* 文本居中 */
+  text-align: center; /* 文本水平居中 */
+  display: flex; /* 使用flex布局 */
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
   top: 70px; /* 第一个 cell 的顶部位置 */
   left: 22px; /* 第一个 cell 的左侧位置 */
 }
@@ -36,8 +60,10 @@ export default {};
   z-index: 2; /* 设置 z-index 高于图片 */
   width: 50px; /* 设置宽度 */
   height: 125px; /* 设置高度 */
-  border: 2px solid #000; /* 添加边框 */
-  text-align: center; /* 文本居中 */
+  text-align: center; /* 文本水平居中 */
+  display: flex; /* 使用flex布局 */
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
   top: 70px; /* 第二个 cell 的顶部位置 */
   left: 134px; /* 第二个 cell 的左侧位置 */
 }
@@ -47,8 +73,10 @@ export default {};
   z-index: 2; /* 设置 z-index 高于图片 */
   width: 50px; /* 设置宽度 */    
   height: 125px; /* 设置高度 */
-  border: 2px solid #000; /* 添加边框 */
-  text-align: center; /* 文本居中 */
+  text-align: center; /* 文本水平居中 */
+  display: flex; /* 使用flex布局 */
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
   top: 70px; /* 第三个 cell 的顶部位置 */
   left: 244px; /* 第三个 cell 的左侧位置 */
 }
